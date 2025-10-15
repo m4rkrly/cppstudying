02acfd0a271d1838a813fc900cb72d90c203ee8b
m4rkrly <w1ltsh1rem4rk@gmail.com>
2025-10-15 16:28:42 +0300

    Refactoring: Part 2

---
+++ 10_10_2025_Refactored/part2/after_refactoring_1.cpp
@@ -0,0 +1,33 @@
+﻿#include <cmath>
+#include <iostream>
+
+void arithMean(const int number, int* array, const int size, int sum); 
+
+int main()
+{
+    const int size = 10;
+    int array[size];
+    int sum = 0;
+   
+    for (int i = 1; i < 4; i++)
+    {
+        arithMean(i, array, size, sum);
+    }
+
+    return 0;
+}
+
+void arithMean(const int number, int* array, const int size, int sum)  
+{
+    std::cout << number << " последовательность" << std::endl;
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = std::rand() % size;
+        std::cout << array[i] << ' ';
+        sum += array[i];
+    }
+    std::cout << std::endl;
+    std::cout << "среднее " << number << " последовательности=" << sum/10 << std::endl;
+}
+
+
