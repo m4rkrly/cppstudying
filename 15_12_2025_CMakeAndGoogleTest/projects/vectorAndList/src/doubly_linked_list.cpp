25a24beb93164264ea10d2daf3c51772e5ef4e79
m4rkrly <w1ltsh1rem4rk@gmail.com>
2026-04-02 19:56:07 +0300

    DLList: Destructor, get_size, has_item and print (with tests)

--- 15_12_2025_CMakeAndGoogleTest/projects/vectorAndList/src/doubly_linked_list.cpp
+++ 15_12_2025_CMakeAndGoogleTest/projects/vectorAndList/src/doubly_linked_list.cpp
@@ -6,22 +6,55 @@ using tkr::DoublyLinkedList;
 
 template<typename T>
 DoublyLinkedList<T>::~DoublyLinkedList() {
-	// TODO
+	if (end != nullptr) {
+		Node* cur = end;
+		while (cur->prev != nullptr) {
+			cur = cur->prev;
+			delete cur->next;
+		}
+		delete cur;
+	}
+	this->begin = nullptr;
+	this->end = nullptr;
 }
 
 template<typename T>
 std::size_t DoublyLinkedList<T>::get_size() const noexcept {
-	// TODO
+	std::size_t counter = 0;
+	if (begin != nullptr) {
+		Node* cur = begin;
+		while (cur != nullptr) {
+			counter++;
+			cur = cur->next;
+		}
+	}
+	return counter;
 }
 
 template<typename T>
 bool DoublyLinkedList<T>::has_item(const T& value) const noexcept {
-	// TODO
+	if (begin != nullptr) {
+		Node* cur = begin;
+		while (cur != nullptr) {
+			if (cur->value == value) 
+				return true;
+			cur = cur->next;
+		}
+	}
+	return false;
 }
 
 template<typename T>
 void DoublyLinkedList<T>::print() const noexcept {
-	// TODO
+	if (begin != nullptr) {
+		Node* cur = begin;
+		std::cout << "[ ";
+		while (cur != nullptr) {
+			std::cout << cur->value << ' ';
+			cur = cur->next;
+		}
+		std::cout << " ]";
+	}
 }
 
 template<typename T>
--- 15_12_2025_CMakeAndGoogleTest/projects/vectorAndList/test/test_doubly_linked_list.cpp
+++ 15_12_2025_CMakeAndGoogleTest/projects/vectorAndList/test/test_doubly_linked_list.cpp
@@ -4,7 +4,14 @@
 
 using tkr::DoublyLinkedList;
 
-// Тесты TODO
+// КОНСТРУКТОР
+TEST(ConstructAndDestruct, Default) {
+	tkr::DoublyLinkedList<int> l1;
+		
+	ASSERT_EQ(l1.get_size(), 0)
+		<< "Неверный размер списка!"
+		<< l1.get_size();
+}
 
 int main(int argc, char **argv) {
 	::testing::InitGoogleTest(&argc, argv);
